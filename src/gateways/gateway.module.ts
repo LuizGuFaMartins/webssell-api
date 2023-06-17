51c539c76a0ce94bba3c20ce373a50b557991ab1
LuizFarabello <luizgufamartins@gmail.com>
2023-06-17 12:23:01 -0300

    fix: gateway module

--- src/gateways/gateway.module.ts
+++ src/gateways/gateway.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItemEntity } from 'src/modules/itens/database/itens.entity';
+import { ItensService } from 'src/modules/itens/itens.service';
 import { ProductEntity } from 'src/modules/products/database/products.entity';
-import { ProductGateway } from './product.gateway';
 import { ProductsService } from 'src/modules/products/products.service';
+import { ItemGateway } from './item.gateway';
+import { ProductGateway } from './product.gateway';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity])],
-  providers: [ProductGateway, ProductsService],
+  imports: [TypeOrmModule.forFeature([ProductEntity, ItemEntity])],
+  providers: [ProductGateway, ProductsService, ItemGateway, ItensService],
 })
 export class GatewayModule {}
